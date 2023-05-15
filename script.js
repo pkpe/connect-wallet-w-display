27a2b66cb1d2affeda13c7e3bf601e85682ed68a
pkpe <94029038+pkpe@users.noreply.github.com>
2023-05-15 05:50:23 -1000

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const connectButton = document.getElementById("myButton");
 // Connect wallet function
 async function connectWallet() {
   try {
-    const wallet = await solana.connect();
+    const wallet = new solanaWeb3.WalletAdapter(window.solana);
+    await wallet.connect();
     publicKey = wallet.publicKey.toString();
     console.log("Connected to wallet:", publicKey);
     connectButton.textContent = "Disconnect Wallet";
@@ -28,8 +29,8 @@ function disconnectWallet() {
 async function updateBalance() {
   if (publicKey) {
     try {
-      const connection = new web3.Connection(web3.clusterApiUrl("mainnet-beta"));
-      const publicKeyObj = new web3.PublicKey(publicKey);
+      const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl("mainnet-beta"));
+      const publicKeyObj = new solanaWeb3.PublicKey(publicKey);
       const balance = await connection.getBalance(publicKeyObj);
       const solBalance = balance / 10 ** 9; // Convert from lamports to SOL
 
@@ -42,25 +43,36 @@ async function updateBalance() {
 }
 
 // Initialize Solana wallet adapter
-const solana = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl("mainnet-beta"));
-
-// Listen for wallet connections
-solana.on("connect", async (wallet) => {
-  publicKey = wallet.publicKey.toString();
-  console.log("Connected to wallet:", publicKey);
-  connectButton.textContent = "Disconnect Wallet";
-  updateBalance();
-});
-
-// Listen for wallet disconnections
-solana.on("disconnect", () => {
-  disconnectWallet();
-});
+window.onload = function () {
+  if ("solana" in window) {
+    const wallet = new solanaWeb3.WalletAdapter(window.solana);
+    solanaWeb3.on("connect", () => {
+      console.log("Connected to wallet provider");
+      connectWallet();
+    });
+    solanaWeb3.on("disconnect", () => {
+      console.log("Disconnected from wallet provider");
+      disconnectWallet();
+    });
+    wallet.on("connect", () => {
+      publicKey = wallet.publicKey.toString();
+      console.log("Connected to wallet:", publicKey);
+      connectButton.textContent = "Disconnect Wallet";
+      updateBalance();
+    });
+    wallet.on("disconnect", () => {
+      disconnectWallet();
+    });
+    wallet.connect();
+  } else {
+    console.error("Solana wallet provider not found");
+  }
+};
 
 // Handle connect/disconnect button clicks
 connectButton.addEventListener("click", () => {
   if (publicKey) {
-    solana.disconnect();
+    solanaWeb3.disconnect();
   } else {
     connectWallet();
   }
